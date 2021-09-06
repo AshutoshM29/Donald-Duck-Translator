9092a4777f3d93f483389601852670a0ad9fafc7
Ashutosh Mishra <ashutoshrm01@gmail.com>
2021-09-06 17:05:54 +0530

    feat : added images and header

--- app.js
+++ app.js
@@ -2,8 +2,7 @@ var txtInput = document.querySelector("#txt-input");
 var btnTranslate = document.querySelector("#btn-translate");
 var outputDiv = document.querySelector("#output");
 
-var serverURL ="https://api.funtranslations.com/translate/ferb-latin.json"
-// var serverURL ="https://api.funtranslations.com/translate/shakespeare.json"
+var serverURL ="https://api.funtranslations.com/translate/dolan.json"
 
 function getTranslationURL(input) {
     return serverURL + "?" + "text=" + input
